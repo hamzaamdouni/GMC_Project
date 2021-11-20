9ddc44951785d2ad33514e3d2454e3914fbdd108
Amdouni Hamza <hamza.amdounitsmm@gmail.com>
2021-11-20 14:44:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.use("/api/agent", require("./router/agent"));
 app.use("/api/user", require("./router/user"));
 app.use("/api/visiteur", require("./router/visiteur"));
 
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("userfront/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "userfront", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT;
 app.listen(PORT, (error) => {
   error
